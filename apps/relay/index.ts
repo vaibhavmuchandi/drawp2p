7b54a50d86461a2fbfe761438ffd24e6d294a1ba
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2023-09-14 18:12:34 +0530

    update:relay - static peerId

--- apps/relay/index.ts
+++ apps/relay/index.ts
@@ -6,11 +6,23 @@ import { webSockets } from '@libp2p/websockets'
 import * as filters from '@libp2p/websockets/filters'
 import { identifyService } from 'libp2p/identify'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import fs from "fs"
+import path from "path"
+import { createFromPrivKey } from "@libp2p/peer-id-factory"
+import { keys } from "@libp2p/crypto";
+
+const __dirname = path.resolve()
 
 const main = async () => {
+    const keyPairJson = JSON.parse(
+        fs.readFileSync(path.join(__dirname, "keypair.json"), "utf-8")
+    );
+    const keyPair = await keys.importKey(keyPairJson.key, "");
+    const peerId = await createFromPrivKey(keyPair);
     const server = await createLibp2p({
+        peerId: peerId,
         addresses: {
-            listen: ['/ip4/127.0.0.1/tcp/0/ws']
+            listen: ['/ip4/127.0.0.1/tcp/57515/ws']
         },
         transports: [
             webSockets({

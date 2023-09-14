47622ab0e48049e50e9d0db584016245fc35677c
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2023-09-14 13:49:35 +0530

    add: pubsub peer discovery for relay

--- apps/relay/index.ts
+++ apps/relay/index.ts
@@ -6,6 +6,7 @@ import { webSockets } from '@libp2p/websockets'
 import * as filters from '@libp2p/websockets/filters'
 import { identifyService } from 'libp2p/identify'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery"
 
 const main = async () => {
     const server = await createLibp2p({
@@ -23,7 +24,10 @@ const main = async () => {
             identify: identifyService(),
             relay: circuitRelayServer(),
             pubsub: gossipsub({ allowPublishToZeroPeers: true })
-        }
+        },
+        peerDiscovery: [
+            pubsubPeerDiscovery()
+        ]
     })
 
     console.log("p2p addr: ", server.getMultiaddrs().map((ma) => ma.toString()))

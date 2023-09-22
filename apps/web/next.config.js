7518fcef8d549d24b4858de0e580ca4e825548b7
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2023-09-22 12:10:52 +0530

    update:web - rewrite

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,5 +12,13 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        "source": "/:roomId*/:peerId*",
+        "destination": "/:roomId*/:peerId*"
+      }
+    ]
   }
 };
